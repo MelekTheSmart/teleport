c1bc11a50ce39d2b4280871239e0380b794d6e4a
Logan Wastlund <loganwastlund@gmail.com>
2021-11-12 13:53:24 -0700

    Everything is green now.. part from 3.

---
+++ maps/Base tile placeholders.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Base tile placeholders" tilewidth="64" tileheight="64" tilecount="20" columns="4">
+ <image source="../Sprites/Base tile placeholders.png" width="256" height="320"/>
+</tileset>
