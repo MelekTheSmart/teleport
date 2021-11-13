38fb2f607b5b8c34c057050c64d6943c14ccdafb
merri <merrickshurtliff@gmail.com>
2021-11-13 07:24:47 -0700

    hmm

--- Layered game/main.py
+++ Layered game/main.py
@@ -17,6 +17,7 @@ def checkbounds(playerrec):
     return check
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
+yevar = 1
 SCREENWIDTH = 1900
 SCREENHEIGHT = 1000
 SCREENSIZE = [SCREENWIDTH, SCREENHEIGHT]
@@ -38,8 +39,9 @@ for object in objects:
     if object.type == "Key":
         keyDoor.append(object)
 doorDoors = tiled_map.get_layer_by_name('placeholder')
+level2Plat = tiled_map.get_object_by_name('Level_2')
 
-
+level3Plat = tiled_map.get_object_by_name('Level_3')
 
 tiles = []
 for x, y, tile in collision.tiles():
@@ -54,61 +56,62 @@ CAMERA = tiled_map.get_object_by_name("player")
 pygame.display.set_caption("My first game in pygame")
 player = pygame.image.load(os.path.join("../Sprites/crown Guy/cguyfront.png")).convert_alpha() # load in coin image, convert_alpha will keep transparent background
 # player = pygame.transform.scale(player, (64, 64)) # resize player image, this should be the same size as the map object
-key = pygame.image.load(os.path.join("../Sprites/Objects/Key.png")).convert_alpha(); # load in coin image, convert_alpha will keep transparent background
+# key = pygame.image.load(os.path.join("../Sprites/Objects/Key.png")).convert_alpha(); # load in coin image, convert_alpha will keep transparent background
+key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-1.png")).convert_alpha();
 # key = pygame.transform.scale(key, (16, 16)) # resize coin image
 door_img = pygame.image.load(os.path.join("../Sprites/doortile.png")).convert_alpha()
-def checkkey(playerrec):
-    for item in keyDoor:
-        itemrec = pygame.Rect([item.x, item.y, item.width, item.height])
-        if (itemrec.colliderect(playerrec) and (item.visible!=0)):
-            item.visible = 0
-def checkkey(playerrec):
-    for item in keyDoor:
-        itemrec = pygame.Rect([item.x, item.y, item.width, item.height])
-        if (itemrec.colliderect(playerrec) and (item.visible!=0) and (item.type=="Key")):
-            item.visible = 0
+door_img = pygame.transform.scale(door_img, (192, 64))
+myObjects = []
+# def checkkey(playerrec):
+for item in keyDoor:
+    myObjects.append(item)
+    # itemrec = pygame.Rect([item.x, item.y, item.width, item.height])
+    # if (itemrec.colliderect(playerrec) and (item.visible!=0)):
+    #     item.visible = 0
+# def checkskey(playerrec):
+#     for item in keyDoor:
+#         myObjects.append(item)
+#         itemrec = pygame.Rect([item.x, item.y, item.width, item.height])
+#         if (itemrec.colliderect(playerrec) and (item.visible!=0) and (item.type=="Key")):
+#             item.visible = 0
+
+
 #game loop
 while True:
     for events in pygame.event.get(): #get all pygame events
         if events.type == pygame.QUIT: #if event is quit then shutdown window and program
             pygame.quit()
             sys.exit()
-    if cycle_key == 1:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-1.png")).convert_alpha();
-    elif cycle_key == 2:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-2.png")).convert_alpha();
-    elif cycle_key == 3:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-3.png")).convert_alpha();
-    elif cycle_key == 4:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-4.png")).convert_alpha();
-    elif cycle_key == 5:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-5.png")).convert_alpha();
-    elif cycle_key == 6:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-6.png")).convert_alpha();
-    elif cycle_key == 7:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-7.png")).convert_alpha();
-    elif cycle_key == 8:
-             key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-8.png")).convert_alpha();
+    # if cycle_key == 1:
+    # elif cycle_key == 2:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-2.png")).convert_alpha();
+    # elif cycle_key == 3:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-3.png")).convert_alpha();
+    # elif cycle_key == 4:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-4.png")).convert_alpha();
+    # elif cycle_key == 5:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-5.png")).convert_alpha();
+    # elif cycle_key == 6:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-6.png")).convert_alpha();
+    # elif cycle_key == 7:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-7.png")).convert_alpha();
+    # elif cycle_key == 8:
+    #          key = pygame.image.load(os.path.join("../Sprites/Objects/keyframes/Key-8.png")).convert_alpha();
     for layer in tiled_map.layers:
-        if isinstance(layer, pytmx.TiledTileLayer): # and (layer != collision):
+        if isinstance(layer, pytmx.TiledTileLayer) and (layer != objects) and (layer != doorDoors): # and (layer != collision):
             for x, y, tile in layer.tiles():
                 if (tile):
                     SCREEN.blit(tile, [(x * tilewidth) - CAMERA.x + (SCREENWIDTH / 2),
                                        (y * tileheight) - CAMERA.y + (SCREENHEIGHT / 2)])
 
-        elif isinstance(layer, pytmx.TiledObjectGroup):
-            for object in layer:
-
-                if (object.name == 'player'):
-                    SCREEN.blit(player,
-                                [object.x - CAMERA.x + (SCREENWIDTH / 2) ,-25 + object.y - CAMERA.y + (SCREENHEIGHT / 2)])
-                elif (object.type == "Key" and key_pickup == 1):
-                    SCREEN.blit(key,
-                                [object.x - CAMERA.x + (SCREENWIDTH / 2),cycle_key + object.y - CAMERA.y + (SCREENHEIGHT / 2)])
-                elif (object.type == "door" and doorDoors.visible == 1 ):
-                    SCREEN.blit(door_img,
-                                [object.x - CAMERA.x + (SCREENWIDTH / 2),
-                                 object.y - CAMERA.y + (SCREENHEIGHT / 2)])
+            # for object in layer:
+            #     if (object.type == "Key" and key_pickup == 1):
+            #         SCREEN.blit(key,
+            #                     [object.x - CAMERA.x + (SCREENWIDTH / 2),cycle_key + object.y - CAMERA.y + (SCREENHEIGHT / 2)])
+            #     elif (object.type == "door" and doorDoors.visible == 1 ):
+            #         SCREEN.blit(door_img,
+            #                     [object.x - CAMERA.x + (SCREENWIDTH / 2),
+            #                      object.y - CAMERA.y + (SCREENHEIGHT / 2)])
 
         pos = [0, 0]
         for events in pygame.event.get():  # get all pygame events
@@ -150,38 +153,58 @@ while True:
 
         playerrec = pygame.Rect([x, y, w, h])
         if playerrec.collidelistall(spaces):
-            pos[1] = -1500
-            pos[0] = 3500.00
+            if yevar == 1:
+                pos[1] = level2Plat.y -  CAMERA.x
+                pos[0] = level2Plat.x - CAMERA.y
+                yevar = 0
+            elif yevar == 0:
+                pos[1] = level3Plat.y + CAMERA.y - 2600
+                pos[0] = level3Plat.x - CAMERA.x
+                yevar = 3
         # check key with the current position of the player
-        checkkey(pygame.Rect([tiled_map.get_object_by_name("player").x, tiled_map.get_object_by_name("player").y,
-                              tiled_map.get_object_by_name("player").width,
-                              tiled_map.get_object_by_name("player").height]))
-        def checkkey(playerrec):
+        def checkkey(aplayerrec):
             for item in keyDoor:
                 itemrec = pygame.Rect([item.x, item.y, item.width, item.height])
-                if (itemrec.colliderect(playerrec) and ( key_pickup == 1 ) and (item.type == "Key")):
+                if (itemrec.colliderect(playerrec)  and (item.type == "Key")):
                     throg = 0
                     DoorsToDisable = item.open
+                    item.visible = 0
                     # Code to get the position of the door and create a rectangle
                     for door in doorDoors:
                         if door.open == DoorsToDisable:
+                            door.visible = 0
                             doorrec = pygame.Rect([door.x, door.y, door.width, door.height])
                             tilestodelete = []  # will add tiles to remove to this list
                             for tile in tiles:
                                 # create rectangle for tile, if it collides with player add this tile to the remove list
-                                tilerec = pygame.Rect([tile.x, tile.y, tile.width, tile.height])
-                                if (doorrec.colliderect(tilerec)):
+                                # tilerec = pygame.Rect([tile.x, tile.y, tile.width, tile.height])
+                                if (doorrec.colliderect(tile)):
                                     tilestodelete.append(tile)
                             # for loop to remove each tile in the remove list
                             for tile in tilestodelete:
                                 tiles.remove(tile)
-                            doorDoors.visible = 0
+                            # doorDoors.visible = 0
+        checkkey(pygame.Rect([tiled_map.get_object_by_name("player").x, tiled_map.get_object_by_name("player").y,
+                              tiled_map.get_object_by_name("player").width,
+                              tiled_map.get_object_by_name("player").height]))
 
         if (checkbounds(playerrec)):
-            pos = [0, 0]
+            # pos = [0, 0]
             me = 1
         tiled_map.get_object_by_name("player").x += pos[0]
         tiled_map.get_object_by_name("player").y += pos[1]
         if can_teleport > 0:
             can_teleport -= 5
+        for object in myObjects:
+            if (object.type == "Key" and object.visible == 1):
+                SCREEN.blit(key,
+                            [object.x - CAMERA.x + (SCREENWIDTH / 2),
+                             cycle_key + object.y - CAMERA.y + (SCREENHEIGHT / 2)])
+            for object in doorDoors:
+                if (object.type == "door" and object.visible == 1):
+                    SCREEN.blit(door_img,
+                                [object.x - CAMERA.x + (SCREENWIDTH / 2),
+                                 object.y - CAMERA.y + (SCREENHEIGHT / 2)])
+            SCREEN.blit(player, [(SCREENWIDTH / 2), -25 + (SCREENHEIGHT / 2)])
+
         pygame.display.update()
