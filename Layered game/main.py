5f484716fcc6b879f32e7bdee24587fb095ec3f1
unknown <merrickshurtliff@gmail.com>
2021-11-12 21:07:25 -0700

    5yt7ty

--- Layered game/main.py
+++ Layered game/main.py
@@ -12,25 +12,30 @@ def checkbounds(playerrec):
     return check
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
-SCREENWIDTH = 500
-SCREENHEIGHT = 500
+SCREENWIDTH = 1900
+SCREENHEIGHT = 1000
 SCREENSIZE = [SCREENWIDTH, SCREENHEIGHT]
 SCREEN = pygame.display.set_mode(SCREENSIZE)
-tiled_map = load_pygame('Something.tmx')
+tiled_map = load_pygame('../maps/map01.tmx')
 tilewidth = tiled_map.tilewidth
 tileheight = tiled_map.tileheight
 collision = tiled_map.get_layer_by_name('collision')
-MOVEMENTSPEED = .5
-
+MOVEMENTSPEED = 5
+COOLDOWN = 100
+can_teleport = COOLDOWN
+TELEPORTDIST = 192
 tiles = []
 for x, y, tile in collision.tiles():
         if (tile):
              tiles.append(pygame.Rect([(x*tilewidth), (y*tileheight), tilewidth, tileheight]));
-CAMERA = tiled_map.get_object_by_name("Player")
+CAMERA = tiled_map.get_object_by_name("player")
 #caption for the game
 pygame.display.set_caption("My first game in pygame")
-player = pygame.image.load(os.path.join("../Sprites/Little Guy 1.0 left.png")).convert_alpha() # load in coin image, convert_alpha will keep transparent background
+player = pygame.image.load(os.path.join("../Sprites/crown Guy/cguyfront.png")).convert_alpha() # load in coin image, convert_alpha will keep transparent background
 # player = pygame.transform.scale(player, (64, 64)) # resize player image, this should be the same size as the map object
+key = pygame.image.load(os.path.join("../Sprites/Objects/Key.png")).convert_alpha(); # load in coin image, convert_alpha will keep transparent background
+# key = pygame.transform.scale(key, (16, 16)) # resize coin image
+
 #game loop
 while True:
     for events in pygame.event.get(): #get all pygame events
@@ -38,7 +43,7 @@ while True:
             pygame.quit()
             sys.exit()
     for layer in tiled_map.layers:
-        if isinstance(layer, pytmx.TiledTileLayer) and (layer != collision):
+        if isinstance(layer, pytmx.TiledTileLayer): # and (layer != collision):
             for x, y, tile in layer.tiles():
                 if (tile):
                     SCREEN.blit(tile, [(x * tilewidth) - CAMERA.x + (SCREENWIDTH / 2),
@@ -46,8 +51,12 @@ while True:
 
         elif isinstance(layer, pytmx.TiledObjectGroup):
             for object in layer:
-                if (object.name == 'Player'):
+
+                if (object.name == 'player'):
                     SCREEN.blit(player,
+                                [object.x - CAMERA.x + (SCREENWIDTH / 2) ,-25 + object.y - CAMERA.y + (SCREENHEIGHT / 2)])
+                elif (object.type == "Key"):
+                    SCREEN.blit(key,
                                 [object.x - CAMERA.x + (SCREENWIDTH / 2), object.y - CAMERA.y + (SCREENHEIGHT / 2)])
         pos = [0, 0]
         for events in pygame.event.get():  # get all pygame events
@@ -57,22 +66,44 @@ while True:
 
         PRESSED = pygame.key.get_pressed()
 
-        if PRESSED[pygame.K_LEFT]:
+        if PRESSED[pygame.K_a]:
             pos[0] -= MOVEMENTSPEED
-        elif PRESSED[pygame.K_RIGHT]:
+        elif PRESSED[pygame.K_d]:
             pos[0] += MOVEMENTSPEED
-        if PRESSED[pygame.K_UP]:
+        if PRESSED[pygame.K_w]:
             pos[1] -= MOVEMENTSPEED
-        elif PRESSED[pygame.K_DOWN]:
+        elif PRESSED[pygame.K_s]:
             pos[1] += MOVEMENTSPEED
-        x = tiled_map.get_object_by_name("Player").x + pos[0]
-        y = tiled_map.get_object_by_name("Player").y + pos[1]
-        w = tiled_map.get_object_by_name("Player").width
-        h = tiled_map.get_object_by_name("Player").height
+        if PRESSED[pygame.K_RIGHT]:
+            if can_teleport < 1:
+                pos[0] += TELEPORTDIST
+                can_teleport = COOLDOWN
+        elif PRESSED[pygame.K_LEFT]:
+            if can_teleport < 1:
+                pos[0] -= TELEPORTDIST
+                can_teleport = COOLDOWN
+        if PRESSED[pygame.K_UP]:
+            if can_teleport < 1:
+                pos[1] =- TELEPORTDIST
+                can_teleport = COOLDOWN
+        elif PRESSED[pygame.K_DOWN]:
+            if can_teleport < 1:
+                pos[1] =+ TELEPORTDIST
+                can_teleport = COOLDOWN
+
+        x = tiled_map.get_object_by_name("player").x + pos[0]
+        y = tiled_map.get_object_by_name("player").y + pos[1]
+        w = tiled_map.get_object_by_name("player").width
+        h = tiled_map.get_object_by_name("player").height
+
         playerrec = pygame.Rect([x, y, w, h])
+
 
         if (checkbounds(playerrec)):
             pos = [0, 0]
-        tiled_map.get_object_by_name("Player").x += pos[0]
-        tiled_map.get_object_by_name("Player").y += pos[1]
+            me = 1
+        tiled_map.get_object_by_name("player").x += pos[0]
+        tiled_map.get_object_by_name("player").y += pos[1]
+        if can_teleport > 0:
+            can_teleport -= 5
         pygame.display.update()
--- maps/donjon tiles.tsx
+++ maps/donjon tiles.tsx
@@ -1,12 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="donjon tiles" tilewidth="64" tileheight="64" tilecount="28" columns="4">
- <image source="../Sprites/donjon tiles.png" width="256" height="448"/>
- <wangsets>
-  <wangset name="Unnamed Set" type="mixed" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
-   <wangtile tileid="13" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="14" wangid="1,1,1,1,1,1,1,1"/>
-  </wangset>
- </wangsets>
+ <image source="../Sprites/tilesets/donjon tiles.png" width="256" height="448"/>
 </tileset>
